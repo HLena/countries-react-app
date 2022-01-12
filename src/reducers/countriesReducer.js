22689b1507d4af74be57f8030a489affa37428b0
ghlena <elena.gordillo@terapygo.com>
2022-01-11 23:02:00 -0500

    Get data from Conuntries API

--- src/reducers/countriesReducer.js
+++ src/reducers/countriesReducer.js
@@ -10,7 +10,8 @@ export const countriesReducer = ( state = initialState, action ) => {
     switch (action.type) {
         case types.countriesLoad:
             return {
-
+                ...state,
+                countries: action.payload,
             }
         default:
             return state;
